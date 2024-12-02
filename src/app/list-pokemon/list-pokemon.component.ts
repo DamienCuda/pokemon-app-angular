87c05f1c139f3aae77bc466135222024c778b84d
Damien Cuda <contact@damiencuda.com>
2024-12-02 11:20:18 +0100

    New component : pokemon list

---
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-pokemon',
+  imports: [],
+  templateUrl: './list-pokemon.component.html',
+  styles: ``
+})
+export class ListPokemonComponent implements OnInit {
+
+  constructor(){}
+
+  ngOnInit(): void { }
+
+}
